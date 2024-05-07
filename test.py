bc1c3cae15d0977b424c376e91e9281325d86d6c
Christopher Quinn <caquinn7@gmail.com>
2024-05-07 17:00:14 -0500

    CLI tests (cli-test-1715119214494)

--- test.py
+++ test.py
@@ -6,7 +6,13 @@ PORT = 6379
 with socket.create_connection((HOST, PORT)) as sock:
     print("Connected successfully!")
     
-    inputs = ["*1\r\n$4\r\nPING\r\n", "*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n"]
+    inputs = [
+        "*1\r\n$4\r\nPING\r\n",
+        "*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n",
+        "*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n",
+        "*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n",
+    ]
+    
     for input in inputs:
         print("Sending:")
         print(input)
