745845b1f167b24b3613a8b6256e03ebd81ac40a
Christopher Quinn <caquinn7@gmail.com>
2024-06-23 12:34:09 -0500

    CLI tests (cli-test-1719164049296)

--- src/binary_utils_ffi.erl
+++ src/binary_utils_ffi.erl
@@ -1,9 +1,31 @@
 -module(binary_utils_ffi).
--export([safe_binary_to_integer/1]).
+-export([
+    safe_binary_to_integer/1,
+    safe_decode_unsigned/1,
+    safe_decode_unsigned_little/1
+]).
 
 safe_binary_to_integer(Bin) ->
     try
         Result = binary_to_integer(Bin),
+        {ok, Result}
+    catch
+        error:badarg ->
+            {error, nil}
+    end.
+
+safe_decode_unsigned(Subject) ->
+    try
+        Result = binary:decode_unsigned(Subject, big),
+        {ok, Result}
+    catch
+        error:badarg ->
+            {error, nil}
+    end.
+
+safe_decode_unsigned_little(Subject) ->
+    try
+        Result = binary:decode_unsigned(Subject, little),
         {ok, Result}
     catch
         error:badarg ->
